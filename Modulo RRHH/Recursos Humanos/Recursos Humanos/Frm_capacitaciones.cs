bafc0d4657401e3f582b57a7d21d113748f0a66b
Jose <jose.ale.gt.umg@gmail.com>
2019-10-15 19:42:59 -0600

    MDI del modulo

---
+++ Modulo RRHH/Recursos Humanos/Recursos Humanos/Frm_capacitaciones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Recursos_Humanos
+{
+    public partial class Frm_capacitaciones : Form
+    {
+        public Frm_capacitaciones()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Modulo RRHH/Recursos Humanos/Recursos Humanos/Mantenimientos/Frm_mantDepartamento.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Recursos_Humanos.Mantenimientos
+{
+    partial class Frm_mantDepartamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Frm_mantDepartamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Frm_mantDepartamento";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm_mantDepartamento";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
